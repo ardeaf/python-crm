a4a5b6b6ff9dc975b6dbee2666e4b9a217dfd692
ardeaf <ardeaf@gmail.com>
2017-04-30 11:19:51 -1000

    Added python 3.5 to setup.py.

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
         'Development Status :: 2 - Pre-Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 )
