fe11d669a08d55ea217d653347357107298613c2
ardeaf <ardeaf@gmail.com>
2017-04-30 21:36:03 -1000

    Moved fixtures to own fixtures.py file.

--- tests/test_crm.py
+++ tests/test_crm.py
@@ -1,77 +1,5 @@
-import os
-from peewee import SqliteDatabase
-import pytest
 from crm.crmmain import Person, Dependent, Application, Preapproval
-from datetime import date
-
-
-@pytest.fixture(scope='session')
-def db():
-    database_path = os.path.join(os.path.expanduser("~"), "test_crm.db")
-    database = SqliteDatabase(database_path)
-    yield database
-    database.drop_tables([Person, Dependent, Application, Preapproval], True)
-
-
-@pytest.fixture()
-def sample_person():
-    return Person(LastName="Lothbrok",
-                  FirstName="Ardeaf",
-                  CellPhone="1234567890",
-                  Email="ardeaf@lothbrok.com",
-                  Birthdate=date(1995, 10, 1),
-                  AddressCurrent="700 Google Dr. #21B, San Mateo, CA, 98102",
-                  AddressMailing="P.O. Box 3, San Mateo, CA, 98102")
-
-
-@pytest.fixture()
-def sample_dependent():
-    return Dependent(LastName="Lothbrok",
-                     FirstName="Bjorn",
-                     Birthdate=date(2005, 6, 1),
-                     Parent=1)
-
-
-@pytest.fixture()
-def sample_application():
-    return Application(Applicant=1,
-                       Date=date(2005, 1, 1),
-                       LoanPurpose="Purchase",
-                       LoanType="Conventional",
-                       CreditScore="750",
-                       Income=5500,
-                       Assets=150000,
-                       FTHB=True,
-                       Locked=True,
-                       LockDate=date(2005, 1, 1),
-                       LockRate=4.250,
-                       LockExpiration=date(2005, 2, 16),
-                       Closed=True,
-                       CloseDate=date(2005, 2, 16),
-                       PurchasePrice=800000,
-                       LoanAmount=600000,
-                       PropertyAddress="800 Microsoft Rd. Suite 500, San Lorenzo, CA, 99122",
-                       MaintenanceFee=452,
-                       Insurance=140,
-                       OtherFees=250)
-
-
-@pytest.fixture()
-def sample_preapproval():
-    return Preapproval(Person=1,                    #This is not an accurate preapproval at all.
-                       MaintenanceFee=600,
-                       Insurance=40,
-                       Taxes=100,
-                       Date=date(2005, 1, 1),
-                       Rate=4.500,
-                       PurchasePrice=400000,
-                       LoanAmount=300000,
-                       CreditScore=650,
-                       Income=5500,
-                       ClosingCosts=5000,
-                       CashToClose=120000,
-                       Reserves=30000,
-                       DTI=45)
+from tests.fixtures import sample_person, db, sample_preapproval, sample_application, sample_dependent
 
 def test_query_matches_saved_person(sample_person, db):
     db.create_table(Person, True)
@@ -125,3 +53,4 @@ def test_create_preapproval_by_querying_name(sample_person, sample_preapproval, 
     sample_preapproval.save()
 
     assert sample_preapproval.Person == Person.get(Person.id == 1)
+
