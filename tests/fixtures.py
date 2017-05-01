0ca90780a949d7801079ffd48dfc086b86ee34aa
ardeaf <ardeaf@gmail.com>
2017-04-30 23:35:34 -1000

    Refactored model attributes (lower-cased), Finished Job model.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,7 +1,7 @@
 import os
 from peewee import SqliteDatabase
 import pytest
-from crm.crmmain import Person, Dependent, Application, Preapproval
+from crm.crmmain import Person, Dependent, Application, Preapproval, Job
 from datetime import date
 
 @pytest.fixture(scope='session')
@@ -9,72 +9,73 @@ def db():
     database_path = os.path.join(os.path.expanduser("~"), "test_crm.db")
     database = SqliteDatabase(database_path)
     yield database
-    database.drop_tables([Person, Dependent, Application, Preapproval], True)
+    database.drop_tables([Person, Dependent, Application, Preapproval, Job], True)
 
 
 @pytest.fixture(scope='module')
 def sample_person():
-    return Person(LastName="Lothbrok",
-                  FirstName="Ardeaf",
-                  CellPhone="1234567890",
-                  Email="ardeaf@lothbrok.com",
-                  Birthdate=date(1995, 10, 1),
-                  AddressCurrent="700 Google Dr. #21B, San Mateo, CA, 98102",
-                  AddressMailing="P.O. Box 3, San Mateo, CA, 98102")
+    return Person(last_name="Lothbrok",
+                  first_name="Ardeaf",
+                  cellphone="1234567890",
+                  email="ardeaf@lothbrok.com",
+                  birthdate=date(1995, 10, 1),
+                  address_current="700 Google Dr. #21B, San Mateo, CA, 98102",
+                  address_mailing="P.O. Box 3, San Mateo, CA, 98102")
 
 
 @pytest.fixture(scope='module')
 def sample_dependent():
-    return Dependent(LastName="Lothbrok",
-                     FirstName="Bjorn",
-                     Birthdate=date(2005, 6, 1),
-                     Parent=1)
+    return Dependent(last_name="Lothbrok",
+                     first_name="Bjorn",
+                     birthdate=date(2005, 6, 1),
+                     parent=1)
 
 
 @pytest.fixture(scope='module')
 def sample_application():
-    return Application(Applicant=1,
-                       Date=date(2005, 1, 1),
-                       LoanPurpose="Purchase",
-                       LoanType="Conventional",
-                       CreditScore="750",
-                       Income=5500,
-                       Assets=150000,
-                       FTHB=True,
-                       Locked=True,
-                       LockDate=date(2005, 1, 1),
-                       LockRate=4.250,
-                       LockExpiration=date(2005, 2, 16),
-                       Closed=True,
-                       CloseDate=date(2005, 2, 16),
-                       PurchasePrice=800000,
-                       LoanAmount=600000,
-                       PropertyAddress="800 Microsoft Rd. Suite 500, San Lorenzo, CA, 99122",
-                       MaintenanceFee=452,
-                       Insurance=140,
-                       OtherFees=250)
+    return Application(person=1,
+                       date=date(2005, 1, 1),
+                       loan_purpose="Purchase",
+                       loan_type="Conventional",
+                       credit_score="750",
+                       income=5500,
+                       assets=150000,
+                       fthb=True,
+                       locked=True,
+                       lock_date=date(2005, 1, 1),
+                       lock_rate=4.250,
+                       lock_expiration=date(2005, 2, 16),
+                       closed=True,
+                       close_date=date(2005, 2, 16),
+                       purchase_price=800000,
+                       loan_amount=600000,
+                       property_address="800 Microsoft Rd. Suite 500, San Lorenzo, CA, 99122",
+                       maintenance_fee=452,
+                       insurance=140,
+                       other_fees=250)
 
 
 @pytest.fixture(scope='module')
 def sample_preapproval():
-    return Preapproval(Person=1,                    #This is not an accurate preapproval at all.
-                       MaintenanceFee=600,
-                       Insurance=40,
-                       Taxes=100,
-                       Date=date(2005, 1, 1),
-                       Rate=4.500,
-                       PurchasePrice=400000,
-                       LoanAmount=300000,
-                       CreditScore=650,
-                       Income=5500,
-                       ClosingCosts=5000,
-                       CashToClose=120000,
-                       Reserves=30000,
-                       DTI=45)
+    return Preapproval(person=1,                    #This is not an accurate preapproval at all.
+                       maintenance_fee=600,
+                       insurance=40,
+                       taxes=100,
+                       date=date(2005, 1, 1),
+                       rate=4.500,
+                       purchase_price=400000,
+                       loan_amount=300000,
+                       credit_score=650,
+                       income=5500,
+                       closing_costs=5000,
+                       cash_to_close=120000,
+                       reserves=30000,
+                       dti=45)
 
 @pytest.fixture(scope='module')
-def sample_employer():
-    return Employment(Person=1,
-                      Employer="Riot Games",
-                      Position="Developer",
-                      MonthlyIncome=5500)
+def sample_job():
+    return Job(person=1,
+               application=1,
+               employer="Riot Games",
+               position="Developer",
+               monthly_income=5500)
--- tests/test_crm.py
+++ tests/test_crm.py
@@ -1,10 +1,10 @@
-from crm.crmmain import Person, Dependent, Application, Preapproval
-from tests.fixtures import sample_person, db, sample_preapproval, sample_application, sample_dependent
+from crm.crmmain import Person, Dependent, Application, Preapproval, Job
+from tests.fixtures import sample_person, db, sample_preapproval, sample_application, sample_dependent, sample_job
 
 def test_query_matches_saved_person(sample_person, db):
     db.create_table(Person, True)
     sample_person.save()
-    assert sample_person.FirstName == Person.get(Person.LastName == sample_person.LastName).FirstName
+    assert sample_person.first_name == Person.get(Person.last_name == sample_person.last_name).first_name
 
 
 def test_dependent_parent_equals_person_from_query(sample_dependent, sample_person, db):
@@ -14,7 +14,7 @@ def test_dependent_parent_equals_person_from_query(sample_dependent, sample_pers
     db.create_table(Dependent, True)
     sample_dependent.save()
 
-    assert sample_dependent.Parent == Person.get(Person.id == 1)  # id is 1 since we added only 1 person
+    assert sample_dependent.parent == Person.get(Person.id == 1)  # id is 1 since we added only 1 person
 
 
 def test_application_applicant_equals_person_from_query(sample_person, sample_application, db):
@@ -24,46 +24,50 @@ def test_application_applicant_equals_person_from_query(sample_person, sample_ap
     db.create_table(Application, True)
     sample_application.save()
 
-    assert sample_application.Applicant == Person.get(Person.id == 1)  # id is 1 since we added only 1 person
+    assert sample_application.person == Person.get(Person.id == 1)  # id is 1 since we added only 1 person
 
 
 def test_create_application_by_querying_name(sample_person, sample_application, db):
     db.create_table(Person, True)
     sample_person.save()
 
-    first_name = Person.get(Person.FirstName == sample_person.FirstName).FirstName
-    last_name = Person.get(Person.LastName == sample_person.LastName).LastName
+    first_name = Person.get(Person.first_name == sample_person.first_name).first_name
+    last_name = Person.get(Person.last_name == sample_person.last_name).last_name
 
     db.create_table(Application, True)
-    sample_application.Applicant == Person.get(Person.FirstName == first_name, Person.LastName == last_name)
+    sample_application.person == Person.get(Person.first_name == first_name, Person.last_name == last_name)
     sample_application.save()
 
-    assert sample_application.Applicant == Person.get(Person.id == 1)
+    assert sample_application.person == Person.get(Person.id == 1)
 
 
 def test_create_preapproval_by_querying_name(sample_person, sample_preapproval, db):
     db.create_table(Person, True)
     sample_person.save()
 
-    first_name = Person.get(Person.FirstName == sample_person.FirstName).FirstName
-    last_name = Person.get(Person.LastName == sample_person.LastName).LastName
+    first_name = Person.get(Person.first_name == sample_person.first_name).first_name
+    last_name = Person.get(Person.last_name == sample_person.last_name).last_name
 
     db.create_table(Preapproval, True)
-    sample_preapproval.Person == Person.get(Person.FirstName == first_name, Person.LastName == last_name)
+    sample_preapproval.person == Person.get(Person.first_name == first_name, Person.last_name == last_name)
     sample_preapproval.save()
 
-    assert sample_preapproval.Person == Person.get(Person.id == 1)
+    assert sample_preapproval.person == Person.get(Person.id == 1)
 
 
-def test_employer_creation_by_querying_name(sample_person, sample_application, db):
+def test_job_creation_by_querying_name(sample_person, sample_application, sample_job, db):
     db.create_table(Person, True)
     sample_person.save()
 
-    first_name = Person.get(Person.FirstName == sample_person.FirstName).FirstName
-    last_name = Person.get(Person.LastName == sample_person.LastName).LastName
+    first_name = Person.get(Person.first_name == sample_person.first_name).first_name
+    last_name = Person.get(Person.last_name == sample_person.last_name).last_name
 
-    db.create_table(Employer, True)
-    sample_employer.Person == Person.get(Person.FirstName == first_name, Person.LastName == last_name)
-    sample_employer.save()
+    db.create_table(Application, True)
+    sample_application.save()
 
-    assert sample_employer.Person == Person.get(Person.id == 1)
+    db.create_table(Job, True)
+    sample_job.Application = Person.get((Person.first_name == sample_person.first_name) &
+                                        (Person.last_name == sample_person.last_name)).applications
+    sample_job.save()
+
+    assert sample_job.person == Person.get(Person.id == 1)
