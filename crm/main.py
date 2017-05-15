cfb28ed7ed962511a95d6a706898f4f6ed4121b6
ardeaf <ardeaf@gmail.com>
2017-05-15 12:51:30 -1000

    Midway through implementing first function in main.py

--- crm/main.py
+++ crm/main.py
@@ -1,12 +1,19 @@
 import os
 from crm.models import Person, Dependent, Application, Preapproval, Job, Asset, Rental, Referral, Communication
 from crm.models import db
+import peewee
+from datetime import date
 import cmd
 
 
-def insert(db):
+def insert(db, object):
     # Inserts a specified model into the db.
-    pass
+    with db.atomic() as txn:
+        for field, field_name in zip(object._meta.sorted_fields, object._meta.sorted_field_names):
+            if isinstance(field, peewee.IntegerField) and not isinstance(field, peewee.PrimaryKeyField) and not isinstance(field, peewee.ForeignKeyField):
+                if getattr(object, field_name) > 2**62:
+                    setattr(object, field_name, str(field))
+        object.save()
 
 
 def remove():
@@ -19,6 +26,7 @@ def main():
     db_name = "crm.db"
     db_path = os.path.join(os.path.expanduser("~"), db_name)
     db.init(db_path)
+
 
 if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,57 @@
 import os
 
-from tests.fixtures import db_fixture
+from tests.fixtures import db_fixture, sample_application
 from crm.models import Person, Dependent, Application, Preapproval, Job, Asset, Rental, Referral, Communication
 from crm import main
-from hypothesis import given
-import hypothesis.strategies as st
+from peewee import fn, CharField, DateField, BooleanField, IntegerField, DecimalField, TextField, ForeignKeyField
+from hypothesis import given, settings, event, assume
+from hypothesis.strategies import text, booleans, integers, builds, decimals, none
+from hypothesis.extra.datetime import dates
 
-@given(st.builds(Person,
-                 last_name=st.text(), first_name=st.text(), cellphone=st.text(), email=st.text(), birthdate=st.text(),
-                 address_current=st.text(), address_mailing=st.text(), is_realtor=st.booleans()))
-def test_insert(db_fixture, person):
+
+# Helper function. Returns dict of field names and the corresponding field type, given an object.
+def fields_to_dict(obj):
+    diction =  {field_name: hypothesis_strategy(field) for field_name, field in zip(obj._meta.sorted_field_names, obj._meta.sorted_fields)
+            if field_name != 'id'}
+
+    return diction
+
+# Helper function.  Reads the given field type and returns the associated hypothesis strategy we are going to use.
+def hypothesis_strategy(field_type):
+    if isinstance(field_type, ForeignKeyField):
+        return none()
+    if isinstance(field_type, CharField) or isinstance(field_type, TextField):
+        return text()
+    if isinstance(field_type, DateField):
+        return dates()
+    if isinstance(field_type, BooleanField):
+        return booleans()
+    if isinstance(field_type, IntegerField):
+        return integers()
+    if isinstance(field_type, DecimalField):
+        return decimals()
+
+@given(builds(Person, **fields_to_dict(Person)),
+       builds(Dependent, **fields_to_dict(Dependent)),
+       builds(Application, **fields_to_dict(Application)))
+def test_insert(db_fixture, person, dependent, application):
     with db_fixture.atomic() as txn:
-        person.create_table(type(person))
-        before_count = person.select().count()
-        main.insert(person, db_fixture)
-        after_count = person.select().count()
-        assert before_count == after_count - 1
-        txn.rollback()
+        person.create_table(True)
+        dependent.create_table(True)
+        application.create_table(True)
+
+        before_count = (person.select().count(), dependent.select().count(), application.select().count())
+
+        main.insert(db_fixture, person)
+
+        random_person = person.select().order_by(fn.Random()).limit(1).get()
+        dependent.parent = random_person
+        application.person = random_person
+
+        main.insert(db_fixture, dependent)
+        main.insert(db_fixture, application)
+
+        after_count = (person.select().count(), dependent.select().count(), application.select().count())
+
+        for counts in zip(before_count, after_count):
+            assert counts[0] == counts[1] - 1
